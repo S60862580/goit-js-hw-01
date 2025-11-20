7149d3fd31a7a76ca63fe9ce5d7958285eefe06d
Sofia <nagornasofia13@gmail.com>
2025-11-20 21:12:06 +0000

    add tasks java

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const contentNumb = parseFloat(content);
+  const paddingNumb = parseFloat(padding);
+  const borderNumb = parseFloat(border);
+  return contentNumb + paddingNumb * 2 + borderNumb * 2;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
